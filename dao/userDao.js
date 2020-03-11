c84beffe7903658d7829456d34dc246c9b36daab
sj-zhong <61337703+sj-zhong@users.noreply.github.com>
2020-03-11 22:58:05 +0900

    Add files via upload
    check value

--- dao/userDao.js
+++ dao/userDao.js
@@ -19,14 +19,72 @@ var jsonWrite = function (res, ret) {
 
 module.exports = {
     add: function (req, res, next) {
+
+       var result={code:0,msg:'s'};
+        if(req.query.name.length === 0 || req.query.name.length > 255)
+        {
+            result = {
+                code: 1,
+                msg:'名前不正：空欄かまたは255文字超えました!'
+            };
+            jsonWrite(res, result);
+            return;
+        }
+
+        if(req.query.age < 0 || req.query.age > 200)
+        {
+            result = {
+                code: 2,
+                msg:'年齢不正!'
+            };
+            jsonWrite(res, result);
+            return;
+        }
+        if(req.query.sex !== "F" && req.query.sex !== "M")
+        {
+            result = {
+                code: 3,
+                msg:'性別不正!'
+            };
+            jsonWrite(res, result);
+            return;
+        }
+        var reg=/^[0-9]{11}$/;
+        if(!reg.test(req.query.telephone))
+        {
+            result = {
+                code: 4,
+                msg:'電話番号不正!'
+            };
+            jsonWrite(res, result);
+            return;
+        }
+
+        var reg=/^[0-9]{7}$/;
+        if(!reg.test(req.query.postNum))
+        {
+            result = {
+                code: 5,
+                msg:'郵便番号不正!'
+            };
+            jsonWrite(res, result);
+            return;
+        }
+        if(req.query.address.length === 0 || req.query.address.length > 255)
+        {
+            result = {
+                code: 6,
+                msg:'住所不正：空欄かまたは255文字超えました！'
+            };
+            jsonWrite(res, result);
+            return;
+        }
         pool.getConnection(function(err, connection) {
             // clientデータ取得
             var param = req.query || req.params;
-			//console.log(param);
 			
             // データベースに該当記録を挿入する
             connection.query($sql.insert, [param.name, param.age, param.sex, param.telephone, param.postNum, param.address], function(err, result) {
-               // console.log("add data result: "+result);
                 if(result) {
                     result = {
                         code: 200,
@@ -42,7 +100,7 @@ module.exports = {
     },
     create: function (req, res, next) {
         pool.getConnection(function(err, connection) {
-	        connection.query("CREATE TABLE user(name  varchar(255),age int, sex varchar(255),telephone decimal(20),postNum decimal(20), address varchar(255))", function(err,result){
+	        connection.query("CREATE TABLE user(name  varchar(255),age int, sex varchar(255),telephone varchar(255),postNum varchar(255), address varchar(255))", function(err,result){
 				if(err){throw err}else{
 					console.log("创建表成功")
 				}
